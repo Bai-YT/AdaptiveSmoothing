Fixed incorrect CIFAR-10 normalization numbers

adaptive_smoothing/utils.py
@@ -10,12 +10,13 @@ import random
 import numpy as np
 import os
 
-from models.base_models import small_rn, trade_wrn, bit_rn, dm_rn
+from models.base_models import small_rn, trades_wrn, bit_rn, dm_rn
 from models.comp_model import CompositeModel
 
 
-def get_comp_model(forward_settings, std_load_path, rob_load_path, num_classes=10):
-
+def get_comp_model(
+    forward_settings, std_load_path, rob_load_path, num_classes=10
+):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     std_model_type = forward_settings["std_model_type"]
     rob_model_type = forward_settings["rob_model_type"]
@@ -35,7 +36,8 @@ def get_comp_model(forward_settings, std_load_path, rob_load_path, num_classes=1
         except:
             std_model = nn.DataParallel(std_model)
             std_model.load_state_dict(
-                torch.load(std_load_path, map_location=device)["model"])
+                torch.load(std_load_path, map_location='cpu')["model"]
+            )
             std_model = std_model.module
     else:
         std_model = small_rn.ResNet18()
@@ -48,22 +50,24 @@ def get_comp_model(forward_settings, std_load_path, rob_load_path, num_classes=1
     assert rob_model_type in ["rn18", "wrn", 'wrn7016', 'wrn7016_silu']
     print(f"Loading robust model: {rob_model_type} from {rob_load_path}...")
 
+    mean = dm_rn.CIFAR100_MEAN if num_classes == 100 else dm_rn.CIFAR10_MEAN
+    std = dm_rn.CIFAR100_STD if num_classes == 100 else dm_rn.CIFAR10_STD
     if rob_model_type == 'wrn7016':
         # Use DeepMind's Swish
         rob_model = dm_rn.WideResNet(
             num_classes=num_classes, activation_fn=dm_rn.Swish,
-            depth=70, width=16, mean=dm_rn.CIFAR100_MEAN, std=dm_rn.CIFAR100_STD
+            depth=70, width=16, mean=mean, std=std
         )
     elif rob_model_type == 'wrn7016_silu':
         # Use PyTorch's SiLU
         rob_model = dm_rn.WideResNet(
             num_classes=num_classes, activation_fn=nn.SiLU, 
-            depth=70, width=16, mean=dm_rn.CIFAR100_MEAN, std=dm_rn.CIFAR100_STD
+            depth=70, width=16, mean=mean, std=std
         )
     elif rob_model_type == "wrn":
-        rob_model = trade_wrn.WideResNet()
+        rob_model = trades_wrn.WideResNet()
     else:
-        rob_model = small_rn.ResNet18()
+        rob_model = small_rn.ResNet18()  # Normalization handled in class init
 
     state_dict = torch.load(rob_load_path)
     if rob_model_type == 'wrn7016_silu':
@@ -86,7 +90,7 @@ def get_comp_model(forward_settings, std_load_path, rob_load_path, num_classes=1
 
 def get_cmodel_and_loaders(
     forward_settings, std_load_path, rob_load_path, dataset_path,
-    dataset_name="CIFAR-10", batch_size_train=500, batch_size_test=500, 
+    dataset_name="CIFAR-10", batch_size_train=500, batch_size_test=500,
     train_with_test=False, train_shuffle=True
 ):
     # Data transforms
@@ -119,13 +123,16 @@ def get_cmodel_and_loaders(
     # Data loaders
     trainloader = DataLoader(
         train_set, batch_size=batch_size_train, pin_memory=True, 
-        shuffle=train_shuffle, num_workers=4, drop_last=True)
+        shuffle=train_shuffle, num_workers=4, drop_last=True
+    )
     trainloader_fast = DataLoader(
         train_set, batch_size=batch_size_train * 9, pin_memory=True, 
-        shuffle=train_shuffle, num_workers=4, drop_last=True)
+        shuffle=train_shuffle, num_workers=4, drop_last=True
+    )
     testloader = DataLoader(
         test_set, batch_size=batch_size_test, pin_memory=True, 
-        shuffle=False, num_workers=4, drop_last=False)
+        shuffle=False, num_workers=4, drop_last=False
+    )
 
     # Composite model
     comp_model = get_comp_model(
@@ -133,17 +140,8 @@ def get_cmodel_and_loaders(
         num_classes=len(test_set.classes)
     )
 
-    return (comp_model, trainloader, testloader, 
-            trainloader_fast, transform_train, transform_test)
-
-
-def process_state_dict_bn(state_dict):
-    # Process state dict (remove "model" prefix)
-    if "bn.running_mean" in state_dict["model"].keys():
-        state_dict["bn"] = OrderedDict()
-        for key in ["running_mean", "running_var", "num_batches_tracked"]:
-            state_dict["bn"][key] = state_dict["model"][f"bn.{key}"]
-            del state_dict["model"][f"bn.{key}"]
+    return comp_model, trainloader, testloader, \
+        trainloader_fast, transform_train, transform_test
 
 
 def load_ckpt(
@@ -156,8 +154,11 @@ def load_ckpt(
     try:  # Optimizer
         optimizer.load_state_dict(state_dict["optimizer"])
         print("Successfully loaded the optimizer state_dict.")
-    except:
-        print("Something went wrong or loading the optimizer!!!")
+    except Exception as err_msg:
+        print("Something went wrong when loading the optimizer!!!")
+        print(f"Error message: {err_msg}")
+        print(optimizer.state_dict()['param_groups'])
+        print(state_dict["optimizer"]['param_groups'])
         print("The optimizer state_dict is not loaded!!!!!\n")
 
     # Scheduler
@@ -184,27 +185,36 @@ def load_ckpt(
         print("Successfully loaded the grad scaler state_dict.")
 
     # Epoch and batch
-    if state_dict["ba"] is not None:
-        state_dict["ba"] += 1
-        if state_dict["ba"] % batch_per_ep != 0:
+    if state_dict['ba'] is not None:
+        state_dict['ba'] += 1
+        if state_dict['ba'] % batch_per_ep != 0:
             print("Warning: Attempting to load from middle of an epoch.")
             print("Continuing training from a checkpoint only works "
-                  "for loading from end-of-epoch.")
+                  "for loading from end of epoch.")
+            print(f"batch count is {state_dict['ba']} “
+                  f"but batch_per_ep is {batch_per_ep}")
     else:
         print("Please note that continuing training from a checkpoint only "
-              "works for loading from end-of-epoch.")
-    ep_start = 1 if reset_scheduler else (state_dict["ep"] + 1 if
-        state_dict["ba"] is None or state_dict["ba"] % batch_per_ep == 0
-        else state_dict["ep"])
+              "works for loading from end of epoch.")
+    ep_start = 1 if reset_scheduler else (state_dict['ep'] + 1 if
+        state_dict['ba'] is None or state_dict['ba'] % batch_per_ep == 0
+        else state_dict['ep'])
 
     # Batch normalization
-    process_state_dict_bn(state_dict)
-    comp_model._comp_model.bn.load_state_dict(state_dict["bn"])
+    # Process state dict (remove "model" prefix)
+    if "bn.running_mean" in state_dict["model"].keys():
+        state_dict['bn'] = OrderedDict()
+        for key in ["running_mean", "running_var", "num_batches_tracked"]:
+            state_dict['bn'][key] = state_dict["model"][f"bn.{key}"]
+            del state_dict["model"][f"bn.{key}"]
+    # Load processed state dict
+    comp_model._comp_model.bn.load_state_dict(state_dict['bn'])
 
     # Gamma scale and bias (after BN)
     if "gamma_scale" in state_dict.keys():
         comp_model._comp_model.set_gamma_scale_bias(
-            state_dict["gamma_scale"], state_dict["gamma_bias"])
+            state_dict["gamma_scale"], state_dict["gamma_bias"]
+        )
     print(f"Gamma scale: {comp_model._comp_model.gamma_scale.item()}, "
           f"Gamma bias: {comp_model._comp_model.gamma_bias.item()}.\n")
 
@@ -213,6 +223,7 @@ def load_ckpt(
 
     # Enable BN affine (default is False)
     if enable_BN:
+        print("Enabled BN affine layer training.")
         comp_model.policy_net.module.bn.affine = True
         comp_model.policy_net.module.bn.weight = \
             nn.parameter.Parameter(torch.empty((1,), device=device))
@@ -220,7 +231,8 @@ def load_ckpt(
             nn.parameter.Parameter(torch.empty((1,), device=device))
         comp_model.policy_net.module.bn.reset_parameters()
         optimizer.add_param_group(
-            {'params': comp_model.policy_net.module.bn.parameters()})
+            {'params': comp_model.policy_net.module.bn.parameters()}
+        )
 
     # Pre-attacked image indices
     img_inds = state_dict["img_inds"]

models/base_models/dm_rn.py
@@ -106,16 +106,17 @@ class _BlockGroup(nn.Module):
 class WideResNet(nn.Module):
     """WideResNet."""
 
-    def __init__(self,
-                 num_classes: int = 10,
-                 depth: int = 28,
-                 width: int = 10,
-                 activation_fn: nn.Module = nn.ReLU,
-                 mean: Union[Tuple[float, ...], float] = CIFAR10_MEAN,
-                 std: Union[Tuple[float, ...], float] = CIFAR10_STD,
-                 padding: int = 0,
-                 num_input_channels: int = 3):
-
+    def __init__(
+        self,
+        num_classes: int = 10,
+        depth: int = 28,
+        width: int = 10,
+        activation_fn: nn.Module = nn.ReLU,
+        mean: Union[Tuple[float, ...], float] = CIFAR10_MEAN,
+        std: Union[Tuple[float, ...], float] = CIFAR10_STD,
+        padding: int = 0,
+        num_input_channels: int = 3
+    ):
         super().__init__()
         self.mean = torch.tensor(mean).view(num_input_channels, 1, 1)
         self.std = torch.tensor(std).view(num_input_channels, 1, 1)

models/base_models/trades_wrn.py
@@ -10,14 +10,19 @@ class WRNBasicBlock(nn.Module):
         super().__init__()
         self.bn1 = nn.BatchNorm2d(in_planes)
         self.relu1 = nn.ReLU(inplace=True)
-        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(
+            in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False
+        )
         self.bn2 = nn.BatchNorm2d(out_planes)
         self.relu2 = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(
+            out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False
+        )
         self.droprate = dropRate
         self.equalInOut = (in_planes == out_planes)
-        self.convShortcut = (not self.equalInOut) and nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride,
-                                                                padding=0, bias=False) or None
+        self.convShortcut = (not self.equalInOut) and nn.Conv2d(
+            in_planes, out_planes, kernel_size=1, stride=stride, padding=0, bias=False
+        ) or None
 
     def forward(self, x):
         if not self.equalInOut:
@@ -39,7 +44,9 @@ class NetworkBlock(nn.Module):
     def _make_layer(self, block, in_planes, out_planes, nb_layers, stride, dropRate):
         layers = []
         for i in range(int(nb_layers)):
-            layers.append(block(i == 0 and in_planes or out_planes, out_planes, i == 0 and stride or 1, dropRate))
+            layers.append(block(
+                i == 0 and in_planes or out_planes, out_planes, i == 0 and stride or 1, dropRate
+            ))
         return nn.Sequential(*layers)
 
     def forward(self, x):
@@ -54,8 +61,9 @@ class WideResNet(nn.Module):
         n = (depth - 4) / 6
         block = WRNBasicBlock
         # 1st conv before any network block
-        self.conv1 = nn.Conv2d(3, nChannels[0], kernel_size=3, stride=1,
-                               padding=1, bias=False)
+        self.conv1 = nn.Conv2d(
+            3, nChannels[0], kernel_size=3, stride=1, padding=1, bias=False
+        )
         # 1st block
         self.block1 = NetworkBlock(n, nChannels[0], nChannels[1], block, 1, drop_rate)
         # 1st sub-block

models/comp_model.py
@@ -37,10 +37,11 @@ class CompositeModel(nn.Module):
         elif self.pn_version == 4:
             self.policy_net = MixingNetV4(forward_settings)
         else:
-            raise "Unsupported mixing network version."
+            raise ValueError("Unsupported mixing network version.")
 
         self.policy_net = (
-            self.policy_net.cuda() if forward_settings["parallel"] == 0 else self.policy_net)
+            self.policy_net.cuda() if forward_settings["parallel"] == 0 else self.policy_net
+        )
         print("The mixing network has "
               f"{sum(p.numel() for p in self.policy_net.parameters())} parameters. "
               f"{sum(p.numel() for p in self.policy_net.parameters() if p.requires_grad)}"
@@ -52,30 +53,37 @@ class CompositeModel(nn.Module):
 
         # Set gamma and the use_policy flag
         self.use_policy = forward_settings["use_policy"]
-        self.set_gamma_value(forward_settings["gamma"])  # Only useful without policy network
+        self.set_gamma_value(forward_settings["gamma"])  # Only useful w/o policy network
         self.scale_alpha = not self.training
 
         # Gamma and alpha scale and bias
-        self.gamma = -np.inf
-        self.gamma_scale = nn.parameter.Parameter(torch.tensor(1.), requires_grad=False)  
+        self.gamma = nn.parameter.Parameter(torch.tensor(-np.inf), requires_grad=False)
+        self.gamma_scale = nn.parameter.Parameter(torch.tensor(1.), requires_grad=False)
         self.gamma_bias = nn.parameter.Parameter(torch.tensor(0.), requires_grad=False)
         self.alpha_scale = nn.parameter.Parameter(torch.tensor(1.), requires_grad=False)
         self.alpha_bias = nn.parameter.Parameter(torch.tensor(0.), requires_grad=False)
         self.std_scale = nn.parameter.Parameter(torch.tensor(1.), requires_grad=False)
         self.rob_scale = nn.parameter.Parameter(torch.tensor(1.), requires_grad=False)
 
-        if "alpha_scale" in forward_settings.keys() and "alpha_bias" in forward_settings.keys():
-            self.set_alpha_scale_bias(forward_settings["alpha_scale"], forward_settings["alpha_bias"])
-        if "std_scale" in forward_settings.keys() and "rob_scale" in forward_settings.keys():
-            self.set_base_model_scale(forward_settings["std_scale"], forward_settings["rob_scale"])
+        if "alpha_scale" in forward_settings.keys() and \
+            "alpha_bias" in forward_settings.keys():
+            self.set_alpha_scale_bias(
+                forward_settings["alpha_scale"], forward_settings["alpha_bias"]
+            )
+        if "std_scale" in forward_settings.keys() and \
+            "rob_scale" in forward_settings.keys():
+            self.set_base_model_scale(
+                forward_settings["std_scale"], forward_settings["rob_scale"]
+            )
 
     def train(self, mode: bool=True, scale_alpha: bool=None):
-        """Sets the mixing network and the BN in training mode. Overloads the train method of nn.Module.
+        """ Sets the mixing network and the BN in training mode.
+            Overloads the train method of nn.Module.
         Args:
-            mode (bool):        Whether to set training mode (``True``) or evaluation mode (``False``). 
-                                Default: ``True``.
+            mode (bool):        Whether to set training mode (True) or eval mode (False). 
+                                Default: True.
             scale_alpha (bool): Whether to scale alpha produced by the mixing network. 
-                                If ``None``, then scale alpha iff in eval mode. Default: ``None``.
+                                If None, then scale alpha iff in eval mode. Default: None.
         """
         if not isinstance(mode, bool):
             raise ValueError("Training mode is expected to be boolean")
@@ -84,8 +92,8 @@ class CompositeModel(nn.Module):
         self.policy_net.train(mode)
         self.bn.train(mode)
 
-        if scale_alpha is None:
-            scale_alpha = not mode  # Default setting is to scale gamma iff in evaluation mode.
+        if scale_alpha is None:  # Default is to scale gamma iff in evaluation mode.
+            scale_alpha = not mode
         self.scale_alpha = scale_alpha
         return self
 
@@ -93,67 +101,81 @@ class CompositeModel(nn.Module):
         return self.train(mode=False, scale_alpha=scale_alpha)
 
     def set_gamma_value(self, gamma):
+        self.gamma = nn.parameter.Parameter(torch.tensor(gamma), requires_grad=False)
         if self.use_policy:
-            self.gamma = gamma
-            print(f"gamma has been set to {self.gamma}, "
+            print(f"gamma has been set to {self.gamma.item()}, "
                   "but the mixing network is active so the change is not effective.")
         else:
-            self.gamma = gamma
-            print(f"Using fixed gamma={self.gamma}. No mixing network.")
-            if self.gamma == -np.inf:
+            print(f"Using fixed gamma={self.gamma.item()}. No mixing network.")
+            print(f"This corresponds to alpha={self.sigmoid(self.gamma).item()}.")
+            if self.gamma.item() == -np.inf:
                 print("Using the STD network only.")
-            elif self.gamma == np.inf:
+            elif self.gamma.item() == np.inf:
                 print("Using the ROB network only.")
 
     def set_gamma_scale_bias(self, gamma_scale, gamma_bias):
         device = self.gamma_bias.device
         self.gamma_bias = nn.parameter.Parameter(
-            torch.tensor(gamma_bias, device=device).float(), requires_grad=False)
-        print(f"The mixing network's gamma mean is set to {self.gamma_bias.item()}.")
+            torch.tensor(gamma_bias, device=device).float(), requires_grad=False
+        )
+        print(f"The mixing network's gamma mean is set to {self.gamma_bias.item():.3}.")
         self.gamma_scale = nn.parameter.Parameter(
-            torch.tensor(gamma_scale, device=device).float(), requires_grad=False)
-        print(f"The mixing network's gamma standard deviation is set to {self.gamma_scale.item()}.")
+            torch.tensor(gamma_scale, device=device).float(), requires_grad=False
+        )
+        print("The mixing network's gamma standard deviation "
+              f"is set to {self.gamma_scale.item():.3}.")
 
     def set_alpha_scale_bias(self, alpha_scale, alpha_bias):
         assert alpha_bias >= 0, "The range of alpha cannot be negative."
         assert alpha_scale + alpha_bias <= 1, "The range of alpha cannot exceed 1."
+
         device = self.alpha_bias.device
         self.alpha_bias = nn.parameter.Parameter(
-            torch.tensor(alpha_bias, device=device).float(), requires_grad=False)
+            torch.tensor(alpha_bias, device=device).float(), requires_grad=False
+        )
         self.alpha_scale = nn.parameter.Parameter(
-            torch.tensor(alpha_scale, device=device).float(), requires_grad=False)
+            torch.tensor(alpha_scale, device=device).float(), requires_grad=False
+        )
         print("The range of alpha during evaluation is set to "
-              f"({self.alpha_bias.item()}, {(self.alpha_bias + self.alpha_scale).item()}).")
+              f"({self.alpha_bias.item():.4}, "
+              f"{(self.alpha_bias + self.alpha_scale).item():.4}).")
 
     def set_base_model_scale(self, std_scale, rob_scale):
         device = self.std_scale.device
         assert std_scale > 0 and rob_scale > 0, \
             "The logit output scale of the base models must be positive."
+
         self.std_scale = nn.parameter.Parameter(
-            torch.tensor(std_scale, device=device).float(), requires_grad=False)
-        print(f"The logit output scale of the STD network is set to {self.std_scale.item()}.")
+            torch.tensor(std_scale, device=device).float(), requires_grad=False
+        )
+        print("The logit output scale of the STD network is set to "
+              f"{self.std_scale.item():.3}.")
         self.rob_scale = nn.parameter.Parameter(
-            torch.tensor(rob_scale, device=device).float(), requires_grad=False)
-        print(f"The logit output scale of the ROB network is set to {self.rob_scale.item()}.")
+            torch.tensor(rob_scale, device=device).float(), requires_grad=False
+        )
+        print("The logit output scale of the ROB network is set to "
+              f"{self.rob_scale.item():.3}.")
 
     def do_checks(self, images):
         if self.policy_graph and not self.use_policy:
-            raise ValueError('policy_graph cannot be created without the mixing network.')
+            raise ValueError(
+                'policy_graph cannot be created without the mixing network.'
+            )
         for model in self.models:
             assert not model.training
 
         if hasattr(self.models[0], "root") and (
             self.models[0].root.conv.weight.device != images.device):
-            print(self.models[0].root.conv.weight.device, 
-                  self.models[1].logits.weight.device, 
-                  self.policy_net.linear.weight.device, 
+            print(self.models[0].root.conv.weight.device,
+                  self.models[1].logits.weight.device,
+                  self.policy_net.linear.weight.device,
                   self.bn.running_mean.device, images.device)
             raise ValueError("Device mismatch!")
 
     def forward(self, images):
         self.do_checks(images)
 
-        # The STD model requires resized images
+        # The STD model may require resized images
         images_resized = F.interpolate(images, size=(128, 128), mode='bilinear') if (
             self.resize and (self.gamma != np.inf or self.use_policy)) else images
 
@@ -179,38 +201,45 @@ class CompositeModel(nn.Module):
             if self.use_policy and self.alpha_scale != 0:  # Use the mixing network
                 if self.policy_graph:
                     gammas = self.policy_net(
-                        [interm_std[0], interm_rob[0]], [interm_std[1], interm_rob[1]])
+                        [interm_std[0], interm_rob[0]], [interm_std[1], interm_rob[1]]
+                    )
                 else:
                     gammas = self.policy_net(
                         [interm_std[0].detach().clone(), interm_rob[0].detach().clone()],
-                        [interm_std[1].detach().clone(), interm_rob[1].detach().clone()])
+                        [interm_std[1].detach().clone(), interm_rob[1].detach().clone()]
+                    )
 
                 # Clamp gammas during training so that the output BN works the best
                 if self.training:
                     amean, astd = gammas.mean().item(), gammas.std().item()
-                    gammas = torch.clamp(gammas, min=(-.6) * astd + amean, max=.6 * astd + amean)
+                    gammas = torch.clamp(
+                        gammas, min=(-.6) * astd + amean, max=.6 * astd + amean
+                    )
 
                 # Apply BN and reparameterize
                 gammas = self.bn(gammas) * self.gamma_scale + self.gamma_bias
-                # print(gammas.mean().item(), gammas.median().item(), (gammas>=0).float().mean().item())
                 alphas = self.sigmoid(gammas)
 
-                # If scale_alpha is specified (default option in eval mode), shrink the range of alphas.
+                # If scale_alpha is specified (default option in eval mode), 
+                # shrink the range of alphas.
                 if self.scale_alpha:
                     alphas = alphas * self.alpha_scale + self.alpha_bias
 
             elif self.use_policy:  # alpha_scale is 0, so use the bias
                 alphas = self.alpha_bias * torch.ones(
-                    (out_data_std.shape[0], 1), device=out_data_std.device)
+                    (out_data_std.shape[0], 1), device=out_data_std.device
+                )
 
             else:  # Use fixed gamma
                 gammas = self.gamma * torch.ones(
-                    (out_data_std.shape[0], 1), device=out_data_std.device)
+                    (out_data_std.shape[0], 1), device=out_data_std.device
+                )
                 alphas = self.sigmoid(gammas)
 
         out_data = torch.log(  # Log is the inverse of the softmax
             (1 - alphas) * self.softmax(out_data_std * self.std_scale) + 
-            alphas * self.softmax(out_data_rob * self.rob_scale))
+            alphas * self.softmax(out_data_rob * self.rob_scale)
+        )
 
         return out_data, gammas.reshape(-1)
 